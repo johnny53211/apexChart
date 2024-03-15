7055277e7e84ff4d9c0987d8d0db520a04998118
johnny53211 <113012359+johnny53211@users.noreply.github.com>
2024-03-15 14:55:48 +0530

    update chart

--- messaging-widget-main/messaging-widget-main/apexChartPlugi.js
+++ messaging-widget-main/messaging-widget-main/apexChartPlugi.js
@@ -1,4 +1,4 @@
-import ApexCharts from './apexcharts/src/apexcharts.js';
+import ApexCharts from './node_modules/apexcharts/src/apexcharts.js';
 let apexChart = {
     "container": document.querySelector("#chart"),
     'init': (args) => {
